c205fe671c4552ced8815f389def9e6a7fe2f21e
Carlos Zeve <carlos.zeve@gmail.com>
2022-03-23 13:35:54 -0300

    Initial commit - Programa Lista de Compras 1.0

---
+++ app/src/main/java/com/aula/lc10/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.aula.lc10
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val produtosAdapter = ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1)
+
+        list_view_produtos.adapter = produtosAdapter
+
+        btn_inserir.setOnClickListener{
+            val produto = edtxt_produto.text.toString()
+            if(produto.isNotEmpty()) {
+                produtosAdapter.add(produto)
+                edtxt_produto.text.clear()
+            }else{
+                edtxt_produto.error = "Preencha um valor!"
+            }
+        }
+
+        list_view_produtos.setOnItemLongClickListener {
+                adapterView: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long ->
+
+            val item = produtosAdapter.getItem(position)
+            produtosAdapter.remove(item)
+            true
+        }
+
+    }
+}
